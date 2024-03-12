195b38a682a0c79be0d25992b529f9cb8bd58930
AnwarRazaa <anwarraza.ra@gmail.com>
2024-03-12 16:26:46 +0500

    Initial Commit

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMOEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'a23b220d-20b6-411f-ba9e-710cf88bebbd';
-export const MODEL_CLOSE_SEC = 3;
+export const MODEL_CLOSE_SEC = 2.5;
